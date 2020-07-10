4071ff4ba82ebce8a632a4ecdc184175752b7f41
Ishan <axil.ishan3@gmail.com>
2020-07-11 02:03:06 +0530

    Added tags and local db

--- app_new/app/src/main/java/com/codechef/ffds/StartActivity.kt
+++ app_new/app/src/main/java/com/codechef/ffds/StartActivity.kt
@@ -2,11 +2,16 @@ package com.codechef.ffds
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.profile_activity.*
 
 class StartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_activity)
+        setContentView(R.layout.profile_activity)
+
+        val tags= listOf("doglover","biker","wanderer","breakingbad","got","royalenfield")
+        tagView.setTags(tags)
+
     }
 }
