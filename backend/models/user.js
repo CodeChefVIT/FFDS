2ad1ce487e14e0a720c99aadad51679126744c0a
NavyaaSharma <navyaasharma700@rediffmail.com>
2020-07-14 23:28:48 +0530

    updated routes

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose')
 var validator = require('validator')
+var bcrypt=require('bcryptjs')
+var jwt=require('jsonwebtoken')
 
-var users = new mongoose.Schema({
+var userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -11,6 +13,7 @@ var users = new mongoose.Schema({
     email: {
         type: String,
         required: true,
+        unique:true,
         trim: true,
         validate(user) {
             if (!validator.isEmail(user)) {
@@ -22,12 +25,15 @@ var users = new mongoose.Schema({
         type:Boolean,
         default:false
     },
+    rand:{
+        type:String
+    },
     password: {
         type: String,
         required: true
     },
     phone:{
-        type:Number
+        type:String
     },
     branch:{
         type:String,
@@ -79,7 +85,41 @@ var users = new mongoose.Schema({
         }
     ]
 })
-
-var userModel = mongoose.model('userModel', users)
+userSchema.methods.toJSON=function(){
+    const user=this
+    const userObject=user.toObject()
+    delete user.password
+    delete user.tokens
+    return userObject
+}
+userSchema.methods.generateToken=async function(){
+    const user=this
+    const token=jwt.sign({_id:user._id.toString()},'mysecret',{expiresIn:'1d'})
+    //user.tokens=user.tokens.concat({token})
+    //await user.save()
+    return token
+}
+userSchema.statics.findByCredentials=async(email,password)=>{
+    const user=await userModel.findOne({email})
+    if(!user)
+    {
+        throw new Error('Unable to login')
+    }
+    const isMatch=await bcrypt.compare(password,user.password)
+    if(!isMatch)
+    {
+        throw new Error('Unable to login')
+    }
+    return user
+}
+userSchema.pre('save',async function(next){
+    const user=this
+    if(user.isModified('password'))
+    {
+        user.password=await bcrypt.hash(user.password,8)
+        next()
+    }
+})
+var userModel = mongoose.model('userModel', userSchema)
 
 module.exports = userModel
