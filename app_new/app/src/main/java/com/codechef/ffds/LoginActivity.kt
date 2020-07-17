e047c16c363bb91d7f5729509935697ecb6798cf
Ishan <axil.ishan3@gmail.com>
2020-07-18 01:06:41 +0530

    Registration #2

---
+++ app_new/app/src/main/java/com/codechef/ffds/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.codechef.ffds
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+    }
+}
