73e14523db23b4cae4d9c6be5382d3458474c827
Sarthak <sarthakbh321@gmail.com>
2020-10-20 23:44:46 +0530

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import MainPage from "./pages/MainPage/MainPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Switch>
+					<Route exact path="/" component={MainPage} />
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/MainPage/MainPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MainPage() {
+	return <div className="main-page">Hello from main page</div>;
+}
+
+export default MainPage;
