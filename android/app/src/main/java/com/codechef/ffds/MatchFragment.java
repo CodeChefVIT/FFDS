4941813e8c0b068b331957c3af070a0278f6256b
Ishan <axil.ishan3@gmail.com>
2021-01-14 02:17:31 +0530

    Added swipe gestures

--- android/app/src/main/java/com/codechef/ffds/MatchFragment.java
+++ android/app/src/main/java/com/codechef/ffds/MatchFragment.java
@@ -1,5 +1,8 @@
 package com.codechef.ffds;
 
+import android.content.Intent;
+import android.graphics.Canvas;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,23 +11,73 @@ import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
 
 public class MatchFragment extends Fragment {
 
-    int position=0;
+    ArrayList<User> matches;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root=inflater.inflate(R.layout.match_activity, container, false);
 
+        matches=new ArrayList<>();
+        matches.add(new User("Ishan","axil.ishan3@gmail.com","Traveller and a hopeless romantic "));
+        matches.add(new User("Shriyashish","axil.ishan3@gmail.com","Sensitive non-weeb who fantasizes vishu "));
+        matches.add(new User("DSP","axil.ishan3@gmail.com","9 pointer but still a stud "));
 
+        RecyclerView recyclerView = root.findViewById(R.id.matches_list);
+        Adapter adapter=new Adapter(getContext(), matches);
+        recyclerView.setAdapter(adapter);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        ItemTouchHelper itemTouchHelper=new ItemTouchHelper(simpleCallback);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
 
         return root;
     }
+
+    ItemTouchHelper.SimpleCallback simpleCallback=new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT | ItemTouchHelper.LEFT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+
+            final int position=viewHolder.getAdapterPosition();
+
+            switch (direction){
+                case ItemTouchHelper.RIGHT:
+                case ItemTouchHelper.LEFT:
+                    break;
+            }
+        }
+
+        @Override
+        public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+
+            new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                    .addBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorPrimaryDark))
+                    .addSwipeRightActionIcon(R.drawable.cupid_icon)
+                    .addSwipeLeftActionIcon(R.drawable.reject_icon)
+                    .create()
+                    .decorate();
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+    };
 }
